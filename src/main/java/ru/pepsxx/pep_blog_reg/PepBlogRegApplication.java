cd96cdb3987b889736dc722494a6019d448a3b83
Pep_$xx <pep_sxx@mail.ru>
2024-11-05 12:53:48 +0300

    f24-001-a01 Hello World

--- src/main/java/ru/pepsxx/pep_blog_reg/PepBlogRegApplication.java
+++ src/main/java/ru/pepsxx/pep_blog_reg/PepBlogRegApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PepBlogRegApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PepBlogRegApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PepBlogRegApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
 
 }

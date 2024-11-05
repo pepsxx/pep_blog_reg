1b70c85ab50e82ee01211a5db35d1882a45698c3
Pep_$xx <pep_sxx@mail.ru>
2024-11-05 12:36:16 +0300

    Initial commit

---
+++ src/main/java/ru/pepsxx/pep_blog_reg/PepBlogRegApplication.java
@@ -0,0 +1,13 @@
+package ru.pepsxx.pep_blog_reg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepBlogRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepBlogRegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pepsxx/pep_blog_reg/PepBlogRegApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pepsxx.pep_blog_reg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepBlogRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

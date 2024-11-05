fcc2e44fd713340be8ff35ba40a179145b98d9f1
Pep_$xx <pep_sxx@mail.ru>
2024-11-05 14:14:42 +0300

    f24-001-a05 Add Lombok

--- src/main/java/ru/pepsxx/pep_blog_reg/PepBlogRegApplication.java
+++ src/main/java/ru/pepsxx/pep_blog_reg/PepBlogRegApplication.java
@@ -1,14 +1,31 @@
 package ru.pepsxx.pep_blog_reg;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class PepBlogRegApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PepBlogRegApplication.class, args);
-        System.out.println("Hello World!");
+        log.info("""
+
+                -----------------------------------------------------------------------------
+                -                                              $$                           -
+                - $$$$$$$$     $$$$$$   $$$$$$$$            $$$$$$$$  $$$$  $$$$ $$$$  $$$$ -
+                - $$$$$$$$$   $$$$$$$$  $$$$$$$$$          $$$$$$$$$$ $$$$  $$$$ $$$$  $$$$ -
+                - $$$$  $$$$ $$$$  $$$$ $$$$  $$$$         $$$$   $$$  $$$  $$$   $$$  $$$  -
+                - $$$$  $$$$ $$$$  $$$$ $$$$  $$$$         $$$$         $$$$$$     $$$$$$   -
+                - $$$$$$$$$  $$$$$$$$$$ $$$$$$$$$          $$$$$$$$$     $$$$       $$$$    -
+                - $$$$$$$$   $$$$$$$$$$ $$$$$$$$            $$$$$$$$$    $$$$       $$$$    -
+                - $$$$       $$$$       $$$$                     $$$$   $$$$$$     $$$$$$   -
+                - $$$$       $$$$  $$$$ $$$$               $$$   $$$$  $$$  $$$   $$$  $$$  -
+                - $$$$        $$$$$$$$  $$$$     $$$$$$$$  $$$$$$$$$$ $$$$  $$$$ $$$$  $$$$ -
+                - $$$$          $$$$    $$$$     $$$$$$$$   $$$$$$$$  $$$$  $$$$ $$$$  $$$$ -
+                -                                              $$                           -
+                -----------------------------------------------------------------------------""");
     }
 
 
